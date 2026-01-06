a8a660dc58cd077d74db521573e53c05cc58ed06
unknown <betranarya03@gmail.com>
2026-01-06 07:27:10 +0700

    update commit

--- config/menu.php
+++ config/menu.php
@@ -12,18 +12,19 @@ return [
     [
         'header' => 'Akun',
         'items' => [
-            ['label' => 'Daftar Akun', 'icon' => 'fa fa-fire', 'route' => 'users.index', 'roles' => ['admin']],
-            ['label' => 'Aktivitas Akun', 'icon' => 'fa fa-water', 'route' => 'users.status', 'roles' => ['admin']],
+            ['label' => 'Daftar Akun', 'icon' => 'fa fa-users', 'route' => 'users.index', 'roles' => ['admin']],
+            ['label' => 'Status Akun', 'icon' => 'fa fa-user-check', 'route' => 'users.status', 'roles' => ['admin']],
+            ['label' => 'Aktivitas Akun', 'icon' => 'fa fa-signal', 'route' => 'users.status', 'roles' => ['admin']],
         ],
     ],
 
     [
         'header' => 'Data Master',
         'items' => [
-            ['label' => 'Aset', 'icon' => 'fa fa-cubes-stacked', 'route' => 'aset.index', 'roles' => ['admin','staf','manager']],
+            ['label' => 'Aset', 'icon' => 'fa fa-cubes', 'route' => 'aset.index', 'roles' => ['admin','staf','manager']],
             ['label' => 'Kategori', 'icon' => 'fa fa-list', 'route' => 'kategori.index', 'roles' => ['admin','staf','manager']],
-            ['label' => 'Lokasi', 'icon' => 'fa fa-school', 'route' => 'lokasi.index', 'roles' => ['admin','staf','manager']],
-            ['label' => 'Karyawan', 'icon' => 'fa fa-copyright', 'route' => 'karyawan.index', 'roles' => ['admin','staf','manager']],
+            ['label' => 'Lokasi', 'icon' => 'fa fa-building', 'route' => 'lokasi.index', 'roles' => ['admin','staf','manager']],
+            ['label' => 'Karyawan', 'icon' => 'fa fa-id-card', 'route' => 'karyawan.index', 'roles' => ['admin','staf','manager']],
         ],
     ],
 
@@ -39,7 +40,7 @@ return [
         'header' => 'Pelaporan',
         'items' => [
             ['label' => 'Pelaporan Aset Masuk', 'icon' => 'fa fa-file-arrow-down', 'route' => 'pelaporan-masuk.index', 'roles' => ['admin','manager']],
-            ['label' => 'Tambah Pelaporan Aset', 'icon' => 'fa fa-file-arrow-down', 'route' => 'tambah-pelaporan.index', 'roles' => ['admin','staf']],
+            ['label' => 'Tambah Pelaporan Aset', 'icon' => 'fa fa-file-circle-plus', 'route' => 'tambah-pelaporan.index', 'roles' => ['admin','staf']],
             ['label' => 'Cek Pelaporan', 'icon' => 'fa fa-file-circle-question', 'route' => 'cek-pelaporan.index', 'roles' => ['staf','manager','admin']],
             ['label' => 'Pelaporan Aset Selesai', 'icon' => 'fa fa-file-circle-check', 'route' => 'pelaporan-selesai.index', 'roles' => ['admin','staf','manager']],
         ],
